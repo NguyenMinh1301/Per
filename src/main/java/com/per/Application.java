a16c02f262fa78befec3c6e60f5d141845c7ab14
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-10-28 14:47:15 +0700

    chore(update): update .env.example, application name

--- src/main/java/com/per/Application.java
+++ src/main/java/com/per/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthApplication {
+public class Application {
 
     public static void main(String[] args) {
+
         System.setProperty("user.timezone", "Asia/Ho_Chi_Minh");
-        SpringApplication.run(AuthApplication.class, args);
+
+        SpringApplication.run(Application.class, args);
     }
 }
