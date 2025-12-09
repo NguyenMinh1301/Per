5b0f09bace3d06a787ad4bf14ce206008120fea2
nguyenminh1301 <nguyenminh1301.dev@gmail.com>
2025-12-09 17:45:21 +0700

    chore(script): add docker environment management scripts

---
+++ scirpt/remove.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ==============================
+# Reset Docker Environment
+# ==============================
+
+echo -e "\n\033[1;34m[1/3] Stopping containers...\033[0m"
+docker stop per postgres redis kafka zookeeper >/dev/null 2>&1
+
+echo -e "\n\033[1;34m[2/3] Removing containers...\033[0m"
+docker rm per postgres redis kafka zookeeper >/dev/null 2>&1
+
+echo -e "\n\033[1;34m[3/3] Removing images...\033[0m"
+docker rmi nguyenminh1301/per:latest \
+  postgres:16-alpine \
+  redis:7-alpine \
+  confluentinc/cp-kafka:7.6.1 \
+  confluentinc/cp-zookeeper:7.6.1 >/dev/null 2>&1
+
+echo -e "\n\033[1;32m Reset completed. All selected containers and images have been removed.\033[0m\n"
