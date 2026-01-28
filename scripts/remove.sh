2b67961f58a1725a919ea4be5f585fd4a69ae535
Nguyen Quang Minh <137589157+NguyenMinh1301@users.noreply.github.com>
2026-01-29 01:12:16 +0700

    chore(scripts): update Docker container and image names in utility scripts

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -5,16 +5,15 @@
 # ==============================
 
 echo -e "\n\033[1;34m[1/3] Stopping containers...\033[0m"
-docker stop per postgres redis kafka zookeeper >/dev/null 2>&1
+docker stop per postgres redis kafka >/dev/null 2>&1
 
 echo -e "\n\033[1;34m[2/3] Removing containers...\033[0m"
-docker rm per postgres redis kafka zookeeper >/dev/null 2>&1
+docker rm per postgres redis kafka >/dev/null 2>&1
 
 echo -e "\n\033[1;34m[3/3] Removing images...\033[0m"
 docker rmi nguyenminh1301/per:latest \
-  postgres:16-alpine \
+  pgvector/pgvector:pg16 \
   redis:7-alpine \
-  confluentinc/cp-kafka:7.6.1 \
-  confluentinc/cp-zookeeper:7.6.1 >/dev/null 2>&1
+  confluentinc/cp-kafka:7.6.1 >/dev/null 2>&1
 
 echo -e "\n\033[1;32m Reset completed. All selected containers and images have been removed.\033[0m\n"
--- scripts/restart.sh
+++ scripts/restart.sh
@@ -5,17 +5,16 @@
 # ==============================
 
 echo -e "\n\033[1;34m[1/4] Stopping containers...\033[0m"
-docker stop per postgres redis kafka zookeeper >/dev/null 2>&1
+docker stop per postgres redis kafka >/dev/null 2>&1
 
 echo -e "\n\033[1;34m[2/4] Removing containers...\033[0m"
-docker rm per postgres redis kafka zookeeper >/dev/null 2>&1
+docker rm per postgres redis kafka >/dev/null 2>&1
 
 echo -e "\n\033[1;34m[3/4] Removing images...\033[0m"
 docker rmi nguyenminh1301/per:latest \
-  postgres:16-alpine \
+  pgvector/pgvector:pg16 \
   redis:7-alpine \
-  confluentinc/cp-kafka:7.6.1 \
-  confluentinc/cp-zookeeper:7.6.1 >/dev/null 2>&1
+  confluentinc/cp-kafka:7.6.1 >/dev/null 2>&1
 
 echo -e "\n\033[1;34m[4/4] Starting Docker Compose...\033[0m"
 docker compose up -d
